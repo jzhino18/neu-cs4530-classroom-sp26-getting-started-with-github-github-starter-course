b87fdba0be33d63d0b9c3c18847940383af74165
Jason Zheng <121064028+jzhino18@users.noreply.github.com>
2026-01-09 16:24:28 -0500

    Implement Counter class with increment method

---
+++ Counter.ts
@@ -0,0 +1,48 @@
+
+
+class Counter {
+
+
+  incrementwithArrow() {
+    setTimeout( () => {       // () => {} wouuld maintain the Counter reference
+        ++;
+      console.log(this.count);
+      
+    }, 100; 
+
+    }
+}
+
+
+  interface async() Counter2
+
+  const Counter = new Counter();
+  counter.incrementWithArrowFunction(); // logging 1
+}
+
+/*
+write some number of function signatures (usually two or more), followed by the body of the function
+
+testing 
+
+classes - extendable?
+classes implement interfaces?
+classes may have getters and setters
+
+members are static and/or readonly/immutable
+members have three main modifiers -> public, protected, and private
+union type can include int and float
+
+if both types are aliases of the same type does this allow
+: type ID = number | string;
+
+--are genetics helpful for http requests? 
+
+https://neu-se.github.io/CS4530-Spring-2026/tutorials/week1-typescript-basics
+
+
+
+
+*/
+
+
